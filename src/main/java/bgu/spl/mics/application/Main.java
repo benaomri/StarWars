2498a05a651ee9237cdd4e1705c726f7a51c5136
benaomri <f3tkktHz>
2020-11-29 20:03:33 +0200

    Structure for Main()

--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,11 +1,31 @@
 package bgu.spl.mics.application;
 
-/** This is the Main class of the application. You should parse the input file, 
+import com.google.gson.Gson;
+import com.sun.org.apache.xml.internal.security.Init;
+
+/** This is the Main class of the application. You should parse the input file,
  * create the different components of the application, and run the system.
  * In the end, you should output a JSON.
  */
 public class Main {
 	public static void main(String[] args) {
+		Init(null);
+		Simulate();
+		Gson out=outGson();
 		
+	}
+
+
+	public static  void Init(Gson gson)
+	{
+
+	}
+	public  static void Simulate()
+	{
+
+	}
+	public static Gson outGson()
+	{
+		return  null;
 	}
 }
